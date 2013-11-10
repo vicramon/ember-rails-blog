d80a382bad2117b985e15d9d837870e4b799feeb
Vic Ramon <vic@vicramon.com>
2013-11-10 15:09:19 -0500

    Show posts via Ember

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,12 @@
+class Api::PostsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Post.all
+  end
+
+  def show
+    respond_with Post.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 EmberRailsBlog::Application.routes.draw do
   root to: 'home#index'
+
+  namespace :api do
+    resources :posts, only: [:index, :show]
+  end
 end
