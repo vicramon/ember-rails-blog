7737e99afbfac84628cfc4ee9817436b635051e9
Vic Ramon <vic@vicramon.com>
2013-11-10 13:43:26 -0500

    Create posts model & migration

---
+++ db/migrate/20131110184251_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.references :user, index: true
+      t.boolean :published, default: false
+
+      t.timestamps
+    end
+  end
+end
