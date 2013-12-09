2555e8c29a6f9a24b6a97fe3bf2c83f7daeee9d1
Vic Ramon <vic@vicramon.com>
2013-12-09 09:59:49 -0500

    Make no file name default to index

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,7 @@ class StaticController < ApplicationController
 
   def show
     file_name = request.params["_escaped_fragment_"]
+    file_name = "index" unless file_name.present?
     render file: "public/static/#{file_name}.html", layout: false
   end
 
